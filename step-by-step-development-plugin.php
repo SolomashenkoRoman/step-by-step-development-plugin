27c9108647a7e0cb5bce1a50413b0200446ba1b4
Roman Solomashenko <romansolomashenko@Romans-Mac-mini.local>
2017-01-16 17:54:18 +0200

    Стандартная структура папок для плагина

--- step-by-step-development-plugin.php
+++ step-by-step-development-plugin.php
@@ -6,6 +6,6 @@ Plugin URI: https://github.com/SolomashenkoRoman/step-by-step-development-plugin
 Description: Step by step development of the plugin.
 Version: 1.0
 Author: romansolomashenko
-Author URI: http://URI_Of_The_Plugin_Author
+Author URI: https://www.linkedin.com/in/solomashenkoroman/
 License: A "Slug" license name e.g. GPL2
 */
---
+++ uninstall.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: romansolomashenko
+ * Date: 16.01.17
+ * Time: 5:49 PM
+ */
