4881cca09c42529cc911223713c1c130def8b265
Roman Solomashenko <romansolomashenko@Romans-Mac-mini.local>
2017-01-17 00:51:52 +0200

    Определение путей: установил константы

--- config-path.php
+++ config-path.php
@@ -5,3 +5,20 @@
  * Date: 16.01.17
  * Time: 9:50 PM
  */
+
+define("STEPBYSTEP_PlUGIN_DIR", plugin_dir_path(__FILE__));
+define("STEPBYSTEP_PlUGIN_URL", plugin_dir_url( __FILE__ ));
+define("STEPBYSTEP_PlUGIN_SLUG", preg_replace( '/[^\da-zA-Z]/i', '_',  basename(STEPBYSTEP_PlUGIN_DIR)));
+define("STEPBYSTEP_PlUGIN_TEXTDOMAIN", str_replace( '_', '-', STEPBYSTEP_PlUGIN_SLUG ));
+define("STEPBYSTEP_PlUGIN_OPTION_VERSION", STEPBYSTEP_PlUGIN_SLUG.'_version');
+define("STEPBYSTEP_PlUGIN_OPTION_NAME", STEPBYSTEP_PlUGIN_SLUG.'_options');
+define("STEPBYSTEP_PlUGIN_AJAX_URL", admin_url('admin-ajax.php'));
+
+if ( ! function_exists( 'get_plugins' ) ) {
+    require_once ABSPATH . 'wp-admin/includes/plugin.php';
+}
+$TPOPlUGINs = get_plugin_data(STEPBYSTEP_PlUGIN_DIR.'/'.basename(STEPBYSTEP_PlUGIN_DIR).'.php', false, false);
+
+define("STEPBYSTEP_PlUGIN_VERSION", $TPOPlUGINs['Version']);
+define("STEPBYSTEP_PlUGIN_NAME", $TPOPlUGINs['Name']);
+
