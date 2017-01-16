3a58b8e5a86f32ac35708f0c1656fdfe840b7bc2
Roman Solomashenko <romansolomashenko@Romans-Mac-mini.local>
2017-01-16 17:28:00 +0200

    Компоновка плагина, Файлы плагина, Заголовки плагина

---
+++ step-by-step-development-plugin.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Step By Step Development Plugin
+Plugin URI: https://github.com/SolomashenkoRoman/step-by-step-development-plugin
+Description: Step by step development of the plugin.
+Version: 1.0
+Author: romansolomashenko
+Author URI: http://URI_Of_The_Plugin_Author
+License: A "Slug" license name e.g. GPL2
+*/
