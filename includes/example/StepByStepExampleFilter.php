dd2cc9571c12e8d508bbb131d3b907f686d0c3fc
Roman Solomashenko <romansolomashenko@Romans-Mac-mini.local>
2017-01-20 12:49:18 +0200

    Пример использования фильтров: класс StepByStepExampleFilter простой пример

--- includes/example/StepByStepExampleFilter.php
+++ includes/example/StepByStepExampleFilter.php
@@ -11,5 +11,28 @@ namespace includes\example;
 
 class StepByStepExampleFilter
 {
+    public function __construct() {
+        //Прикрепляем функцию к фильтру
+        add_filter('my_filter', array(&$this, 'myFiterFunction'));
+    }
+    public static function newInstance(){
+        $instance = new self;
+        return $instance;
+    }
 
+    /**
+     * Функция которую вызовет фильтер
+     * @param $str
+     * @return string
+     */
+    public function myFiterFunction( $str ){
+        $str = "Hello {$str}";
+        return $str;
+    }
+
+    public function callMyFilter( $name ){
+        $name = apply_filters('my_filter', $name);
+        //Выводим результат в debug.log
+        error_log($name);
+    }
 }
