a099748b43d0104c89c3efc3536c3f34a757ea58
SolomashenkoRoman <solomashenko.roman.1991@gmail.com>
2017-01-27 14:41:03 +0200

    Создадим базовый класс контроллер для создания пунктов меню

---
+++ includes/controllers/admin/menu/StepByStepBaseAdminMenuController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: solomashenko
+ * Date: 27.01.17
+ * Time: 14:30
+ */
+
+namespace includes\controllers\admin\menu;
+
+
+abstract class StepByStepBaseAdminMenuController
+{
+    public function __construct(){
+        /*
+         * Регистрирует хук-событие. При регистрации указывается PHP функция,
+         * которая сработает в момент события, которое вызывается с помощью do_action().
+         */
+        add_action('admin_menu', array( &$this, 'action'));
+    }
+
+    abstract public function action();
+    abstract public function render();
+}
