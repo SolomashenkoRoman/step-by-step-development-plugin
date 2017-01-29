55b898fb1afe4769be3d2d200f982cf44bd4f27a
SolomashenkoRoman <solomashenko.roman.1991@gmail.com>
2017-01-29 18:44:29 +0200

    Класс StepByStepMyDashboardMenuController который будет отвечать за добавление дочерней страницы плагина в раздел плагина Dashboard.

--- includes/controllers/admin/menu/StepByStepMyDashboardMenuController.php
+++ includes/controllers/admin/menu/StepByStepMyDashboardMenuController.php
@@ -2,40 +2,41 @@
 /**
  * Created by PhpStorm.
  * User: solomashenko
- * Date: 27.01.17
- * Time: 16:25
+ * Date: 29.01.17
+ * Time: 18:34
  */
 
 namespace includes\controllers\admin\menu;
 
 
-class StepByStepMainAdminSubMenuController extends StepByStepBaseAdminMenuController
+class StepByStepMyDashboardMenuController extends StepByStepBaseAdminMenuController
 {
 
     public function action()
     {
         // TODO: Implement action() method.
-        $pluginPage = add_submenu_page(
+
+        $pluginPage = add_dashboard_page(
             STEPBYSTEP_PlUGIN_TEXTDOMAIN,
             _x(
-                'Sub Step By Step',
+                'Sub dashboard Step By Step',
                 'admin menu page' ,
                 STEPBYSTEP_PlUGIN_TEXTDOMAIN
             ),
             _x(
-                'Sub Step By Step',
+                'Sub dashboard Step By Step',
                 'admin menu page' ,
                 STEPBYSTEP_PlUGIN_TEXTDOMAIN
             ),
             'manage_options',
-            'step_by_step_control_sub_menu',
+            'step_by_step_control_sub_dashboard_menu',
             array(&$this, 'render'));
     }
 
     public function render()
     {
         // TODO: Implement render() method.
-        _e("Hello world sub menu", STEPBYSTEP_PlUGIN_TEXTDOMAIN);
+        _e("Hello this page dashboards", STEPBYSTEP_PlUGIN_TEXTDOMAIN);
     }
 
     public static function newInstance()
