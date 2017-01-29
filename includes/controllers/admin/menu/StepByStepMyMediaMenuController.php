23a782aaa709ffdd8e6cdb5bf992da231dcf058b
SolomashenkoRoman <solomashenko.roman.1991@gmail.com>
2017-01-29 19:10:44 +0200

    Создадим класс StepByStepMyMediaMenuController который будет отвечать за добавление дочерней страницы плагина в раздел плагина Media.

---
+++ includes/controllers/admin/menu/StepByStepMyMediaMenuController.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: solomashenko
+ * Date: 29.01.17
+ * Time: 19:07
+ */
+
+namespace includes\controllers\admin\menu;
+
+
+class StepByStepMyMediaMenuController extends StepByStepBaseAdminMenuController
+{
+    public function action()
+    {
+        // TODO: Implement action() method.
+
+        $pluginPage = add_posts_page(
+            __('Sub media Step By Step', STEPBYSTEP_PlUGIN_TEXTDOMAIN),
+            __('Sub media Step By Step', STEPBYSTEP_PlUGIN_TEXTDOMAIN),
+            'read',
+            'step_by_step_control_sub_media_menu',
+            array(&$this, 'render')
+        );
+    }
+
+    public function render()
+    {
+        // TODO: Implement render() method.
+        _e("Hello this page media", STEPBYSTEP_PlUGIN_TEXTDOMAIN);
+    }
+
+    public static function newInstance()
+    {
+        // TODO: Implement newInstance() method.
+        $instance = new self;
+        return $instance;
+    }
+}
