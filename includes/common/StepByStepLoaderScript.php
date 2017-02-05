1099f09915fc444731f4cdeb7b02f74bb29ebacc
Roman Solomashenko <romansolomashenko@Romans-Mac-mini.local>
2017-02-05 17:30:40 +0200

    Класс отвечающий за добавление JS и CSS в плагине добавил методы на действия которые будут отвечать за загрузку скриптов в определеном месте

--- includes/common/StepByStepLoaderScript.php
+++ includes/common/StepByStepLoaderScript.php
@@ -11,5 +11,31 @@ namespace includes\common;
 
 class StepByStepLoaderScript
 {
+    private static $instance = null;
 
+    private function __construct(){
+        // Проверяем в админке мы или нет
+        if ( is_admin() ) {
+            add_action('admin_enqueue_scripts', array(&$this, 'loadScriptAdmin' ) );
+            add_action('admin_head', array(&$this, 'loadHeadScriptAdmin'));
+        } else {
+            add_action( 'wp_enqueue_scripts', array(&$this, 'loadScriptSite' ) );
+            add_action('wp_head', array(&$this, 'loadHeadScriptSite'));
+            add_action( 'wp_footer', array(&$this, 'loadFooterScriptSite'));
+        }
+
+    }
+
+    public static function getInstance(){
+        if ( null == self::$instance ) {
+            self::$instance = new self;
+        }
+        return self::$instance;
+    }
+
+    public function loadScriptAdmin($hook){}
+    public function loadHeadScriptAdmin(){}
+    public function loadScriptSite($hook){}
+    public function loadHeadScriptSite(){}
+    public function loadFooterScriptSite(){}
 }
