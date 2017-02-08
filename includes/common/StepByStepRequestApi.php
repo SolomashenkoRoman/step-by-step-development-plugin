0d23942bf9812baa91a64464e485764cd72e8f7f
Roman Solomashenko <romansolomashenko@Romans-Mac-mini.local>
2017-02-08 22:06:44 +0200

    Создал класс для работы с API.  Класс singleton

--- includes/common/StepByStepRequestApi.php
+++ includes/common/StepByStepRequestApi.php
@@ -11,5 +11,14 @@ namespace includes\common;
 
 class StepByStepRequestApi
 {
+    private static $instance = null;
+    private function __construct(){
 
+    }
+    public static function getInstance(){
+        if ( null == self::$instance ) {
+            self::$instance = new self;
+        }
+        return self::$instance;
+    }
 }
