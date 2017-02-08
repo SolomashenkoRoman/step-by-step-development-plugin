22cbcf7b4031b6ae56045a0b45df61e37e687f20
Roman Solomashenko <romansolomashenko@Romans-Mac-mini.local>
2017-02-08 22:20:05 +0200

    Создал класс для работы с API.  TOKEN MARKER API travelpayouts

--- includes/common/StepByStepRequestApi.php
+++ includes/common/StepByStepRequestApi.php
@@ -12,7 +12,10 @@ namespace includes\common;
 class StepByStepRequestApi
 {
     const STEPBYSTEP_API_V2 = "http://api.travelpayouts.com/v2";
+    const STEPBYSTEP_TOKEN = "b2f8bef81735323aecb33e285da8e694";
+    const STEPBYSTEP_MARKER = "17942";
     private static $instance = null;
+
     private function __construct(){
 
     }
