d885b86e7bcb025b8b27f038f1335cb520f7e244
SolomashenkoRoman <solomashenko.roman.1991@gmail.com>
2017-02-13 00:01:53 +0200

    StepByStepDefaultOption добавил класс отвечающий за дефолтные настройки добавил метод возвращающий массив дефолтных настроек

--- includes/common/StepByStepDefaultOption.php
+++ includes/common/StepByStepDefaultOption.php
@@ -11,5 +11,15 @@ namespace includes\common;
 
 class StepByStepDefaultOption
 {
-
+    public static function getDefaultOptions()
+    {
+        $defaults = array(
+            'account' => array(
+                'marker' => '',
+                'token' => ''
+            )
+        );
+        $defaults = apply_filters('step_by_step_default_option', $defaults );
+        return $defaults;
+    }
 }
