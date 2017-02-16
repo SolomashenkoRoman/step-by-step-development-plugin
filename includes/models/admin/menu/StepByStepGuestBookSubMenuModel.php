9a0b1b03cb9f07ab3446867dabe579410549174f
SolomashenkoRoman <solomashenko.roman.1991@gmail.com>
2017-02-16 16:09:36 +0200

    Гостевая книга класс модель

---
+++ includes/models/admin/menu/StepByStepGuestBookSubMenuModel.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: solomashenko
+ * Date: 16.02.17
+ * Time: 16:08
+ */
+
+namespace includes\models\admin\menu;
+
+
+class StepByStepGuestBookSubMenuModel
+{
+
+}
