299130a68d1b80c75b448c0c5143015f6889721f
SolomashenkoRoman <solomashenko.roman.1991@gmail.com>
2017-02-16 15:46:12 +0200

    Класс контролер дря пункта меню Гостевая книга

---
+++ includes/controllers/admin/menu/StepByStepGuestBookSubMenuController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: solomashenko
+ * Date: 16.02.17
+ * Time: 15:42
+ */
+
+namespace includes\controllers\admin\menu;
+
+
+class StepByStepGuestBookSubMenuController
+{
+
+}
