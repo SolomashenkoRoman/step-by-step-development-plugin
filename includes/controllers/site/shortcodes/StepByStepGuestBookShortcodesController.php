71e00eea35b8ed4b5e99c2ad5ad94811aedd8bd8
Roman Solomashenko <romansolomashenko@Romans-Mac-mini.local>
2017-02-21 14:38:07 +0200

    Добавляем щорткод [step_by_step_guest_book] подключаем ajax обработчик

--- includes/controllers/site/shortcodes/StepByStepGuestBookShortcodesController.php
+++ includes/controllers/site/shortcodes/StepByStepGuestBookShortcodesController.php
@@ -17,6 +17,31 @@ class StepByStepGuestBookShortcodesController extends StepByStepShortcodesContro
     implements StepByStepICreatorInstance
 {
 
+    // Переопределим конструктор
+    public function __construct(){
+        // Вызов конструктора класса StepByStepShortcodesController
+        // Чтобы прикрепить функцию к действию add_action( 'wp_loaded',  array( &$this, 'initShortcode') );
+        parent::__construct();
+
+        // подключаем AJAX обработчики, только когда в этом есть смысл
+        if( defined('DOING_AJAX') && DOING_AJAX ){
+            add_action('wp_ajax_guest_book', array( &$this, 'ajaxHandler'));
+            add_action('wp_ajax_nopriv_guest_book',  array( &$this, 'ajaxHandler'));
+        }
+
+        // Переменная ajaxurl
+
+
+    }
+
+    /**
+     * Обработчик для ajax действия guest_book (wp_ajax_guest_book, wp_ajax_nopriv_guest_book)
+     */
+    public function ajaxHandler(){
+
+    }
+
+
     /**
      * Функция в которой будем добалять шорткоды через функцию add_shortcode( $tag , $func );
      * @return mixed
@@ -67,12 +92,12 @@ class StepByStepGuestBookShortcodesController extends StepByStepShortcodesContro
         // TODO: Implement render() method.
         // В этой переменой будет view формы
         $output = '';
-        $output .= '<form action="" method="post">
+        $output .= '<form  method="post">
                         <label>'.__('User name', STEPBYSTEP_PlUGIN_TEXTDOMAIN ).'</label>
                         <input type="text" name="step_user_name">
                         <label>'.__('Message', STEPBYSTEP_PlUGIN_TEXTDOMAIN ).'</label>
                         <textarea name="step_message"></textarea>
-                        <button class="step-by-steb-btn-add" >'.__('Add', STEPBYSTEP_PlUGIN_TEXTDOMAIN ).'<button
+                        <button class="step-by-steb-btn-add" >'.__('Add', STEPBYSTEP_PlUGIN_TEXTDOMAIN ).'</button>                   
                     </form>';
         return $output;
     }
